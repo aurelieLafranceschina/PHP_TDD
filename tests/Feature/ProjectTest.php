0e90fcc082e227179dcedb5c49f57ab9d740b03c
aurelieLafranceschina <aurelielafran@gmail.com>
2018-05-16 13:56:41 +0200

    test fonctionnel du statut HTTP

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -1,15 +1,21 @@
 <?php
-/**Contient les tests fonctionnels*/
 
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProjectTest extends TestCase
 {
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
     public function testBasicTest()
     {
-        $response = $this->get('/project');
+        $response = $this->get('http://127.0.0.1:8000/');
 
         $response->assertStatus(200);
     }
